832173d6dab3a1a41517032d9a68d7386bc032d5
Earl Brown <github@theGleep.com>
2022-02-05 18:20:50 -0700

    In the middle of the first lock flap

--- components/generators/ECMA a55.20.01.01.js
+++ components/generators/ECMA a55.20.01.01.js
@@ -13,7 +13,7 @@ export function defineCarton(depth, width, length,) {
 	const bottom = (width * 2) + (depth * 2)
 
 	const flapWidth = width * 0.75
-	const flapDepth = depth * 0.35
+	const flapLength = length * 0.475
 
 	const glueTab = [
 		`M ${offset.x} ${offset.y}`,
@@ -22,51 +22,66 @@ export function defineCarton(depth, width, length,) {
 		`l ${depth * 0.1} ${flapWidth}`,
 	]
 
-	const lockFlap = [
+	// 								rx 							ry 						 x-axis-rotation 	large-arc sweep x									y
+	const arc1 = `a 	${width * 0.1}	${width * 0.1} 0 								0 				0 		${width * -0.1} 	${width * 0.1}`
+
+	const arc2 = `a ${width * 0.1} ${width * 0.1} 0 0 0 ${width * 0.1} ${width * 0.1}`
+
+
+	const lockTab = [
+		`M ${offset.x} ${offset.y}`,
 		`l 	${(width * -0.5)} ${length * 0.2}`,
 		`h 	${(width * -0.4)}`,
-
-		// 	rx 							ry 						 x-axis-rotation 	large-arc sweep x									y
-		`a 	${width * 0.1}	${width * 0.1} 0 								0 				0 		${width * -0.1} 	${width * 0.1}`,
+		arc1,
 
 		`v 	${(length * 0.6) - (width * 0.2)}`,
 
-		`a 	${width * 0.1}	${width * 0.1} 0 								0 				0 		${width * 0.1} 	${width * 0.1}`,
+		arc2,
+
 		`h 	${(width * 0.4)}`,
 		`l  ${width * 0.5} ${length * 0.2}`,
 
 		...glueTab
 	]
 
+	const supportFlap1 = [
+		`M ${offset.x} ${offset.y + length}`,
+		`h ${-flapLength}`,
+		`v ${width * 0.4}`,
+		arc2,
+
+
+	]
+
 	// const overFlap = [
-	// 	`l ${depth * -0.5} ${depth * 0.5}`,
-	// 	`h ${depth * -0.25}`,
+	// 	`l ${ depth * -0.5 } ${ depth * 0.5 } `,
+	// 	`h ${ depth * -0.25 } `,
 
-	// 	`a ${depth * 0.25} ${depth * 0.25} 0 0 0 ${depth * -0.25} ${depth * 0.25}`,
+	// 	`a ${ depth * 0.25 } ${ depth * 0.25 } 0 0 0 ${ depth * -0.25 } ${ depth * 0.25 } `,
 
-	// 	`v ${width - (depth * 1.5)}`,
-	// 	`a ${depth * 0.25} ${depth * 0.25} 0 0 0 ${depth * 0.25} ${depth * 0.25}`,
+	// 	`v ${ width - (depth * 1.5) } `,
+	// 	`a ${ depth * 0.25 } ${ depth * 0.25 } 0 0 0 ${ depth * 0.25 } ${ depth * 0.25 } `,
 
-	// 	`h ${depth * 0.25}`,
-	// 	`l ${depth * 0.5} ${depth * 0.5}`,
+	// 	`h ${ depth * 0.25 } `,
+	// 	`l ${ depth * 0.5 } ${ depth * 0.5 } `,
 	// ]
 
 	// const tuckFlap = [
-	// 	`h ${-depth}`,
-	// 	`v ${width * 0.1}`,
-	// 	`a ${depth * 0.25} ${depth * 0.25} 0 0 0 ${depth * 0.25} ${depth * 0.25}`,
-	// 	`h ${depth * 0.25}`,
+	// 	`h ${ -depth } `,
+	// 	`v ${ width * 0.1 } `,
+	// 	`a ${ depth * 0.25 } ${ depth * 0.25 } 0 0 0 ${ depth * 0.25 } ${ depth * 0.25 } `,
+	// 	`h ${ depth * 0.25 } `,
 
-	// 	`v ${width - (depth * 1)}`,
-	// 	`h ${depth * -0.25}`,
-	// 	`a ${depth * 0.25} ${depth * 0.25} 0 0 0 ${depth * -0.25} ${depth * 0.25}`,
-	// 	`v ${width * 0.1}`,
-	// 	`h ${depth}`
+	// 	`v ${ width - (depth * 1) } `,
+	// 	`h ${ depth * -0.25 } `,
+	// 	`a ${ depth * 0.25 } ${ depth * 0.25 } 0 0 0 ${ depth * -0.25 } ${ depth * 0.25 } `,
+	// 	`v ${ width * 0.1 } `,
+	// 	`h ${ depth } `
 	// ]
 
 	const lidFold = [
-		`M ${offset.x + depth + width} ${offset.y + length + width}`,
-		`v ${length}`,
+		`M ${offset.x + depth + width} ${offset.y + length + width} `,
+		`v ${length} `,
 	]
 
 
@@ -82,16 +97,17 @@ export function defineCarton(depth, width, length,) {
 
 
 		foldLines: [
-			`M ${offset.x} ${offset.y}`,
+			`M ${offset.x} ${offset.y} `,
 			...box,
 			...lidFold,
 			...glueTab
 		],
 
 		cutLines: [
-			`M 	${offset.x} ${offset.y}`,
-			...lockFlap,
-			// ...overFlap,
+			`M 	${offset.x} ${offset.y} `,
+			...lockTab,
+			...supportFlap1,
+
 			// ...downFlap,
 
 			// ...tuckFlap,
